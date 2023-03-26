009865260a59cf1787366a190f4b46f42b78698c
Derek Viera <vierader@msu.edu>
2023-03-26 16:15:03 -0400

    Removed ul and made tables and css for table

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function getBooks() {
 
   for (let book of books) {
     let bookList = document.getElementById('books');
-    bookList.innerHTML += `<li>${book.title} - ${book.author} - ${book.isbn}</li>`;
+    bookList.innerHTML += `<tr><td>${book.title}</td<td>${book.author}</td><td>${book.description}</td></tr>`;
   }
 }
 
