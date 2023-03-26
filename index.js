075269f8d9bfe9e112a8320fce5532996850abbb
Derek Viera <vierader@msu.edu>
2023-03-26 15:54:18 -0400

    Supabase test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add JavaScript below
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.12.0"
+
+const supabaseUrl = 'https://abxjpvfkqrpphlgaiosn.supabase.co'
+
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFieGpwdmZrcXJwcGhsZ2Fpb3NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkzNjAzNDYsImV4cCI6MTk5NDkzNjM0Nn0.0ldX3JshduFq6ccqMOD7w3SgBH4h4kTRdk0yHxgbFvI'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks() {
+  let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+  for (let book of books) {
+    let bookList = document.getElementById('books');
+    bookList.innerHTML += `<li>${book.title} - ${book.author} - ${book.isbn}</li>`;
+  }
+}
+
+getBooks();
